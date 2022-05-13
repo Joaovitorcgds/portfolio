05418f5f185b4713e9a3ff752b0688bb08b9df9b
joaov <joaovitorcgds@gmail.com>
2022-05-13 09:51:19 -0300

    botão mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,31 @@ import React from "react"
 import styled from "styled-components"
 
 
-export default function Header({onScrollToAnimation}){
+export default function Header(){
   function scrollToAnimation(e){
     e.preventDefault()
+    const nav = document.querySelector(".nav")
     const element = e.target
     const id = element.getAttribute("href")
     const to = document.querySelector(id).offsetTop
     window.scroll({
       top: to - 20,
-      behavior: "smooth"})
-    }
+      behavior: "smooth"
+    })
+    nav.classList.remove("active")
+  }
+  function toggleMenu(){
+    const nav = document.querySelector(".nav")
+    nav.classList.toggle("active")
+  }
+
   return (
     <div>
       <HeaderStyle>
         <span>João Vitor Costa</span>
-        <nav>
-          <ul>
+        <nav className="nav">
+          <button onClick={() => toggleMenu()} className="btnMenu">Menu</button>
+          <ul className="menu">
             <li>
               <a href="#about" onClick={scrollToAnimation}>
                 Sobre
@@ -61,6 +70,59 @@ const HeaderStyle = styled.header`
   span{
     font-size: 2.4rem;
     font-weight: 600;
+  }
+
+  .btnMenu{
+      display: none;
+    }
+
+  @media (max-width: 500px) { 
+    .btnMenu{
+      display: block;
+      background: transparent;
+      color: #FBFBFF;
+      border: 1px solid #FBFBFF;
+      padding: 5px 10px;
+      font-weight: 700;
+      transition: .6s;
+    }
+
+
+    .menu{
+      display: block;
+      position: absolute;
+      width: 100%;
+      top: 65px;
+      right: 0px;
+      height: 0px;
+      transition: .6s;
+      visibility: hidden;
+      overflow-y: hidden;
+      background: #150F43;
+      z-index: 1000;
+    }
+
+    .nav.active .menu{
+      height: calc(100vh - 65px);
+      visibility: visible;
+      overflow-y: auto;
+    }
+
+    .nav.active .btnMenu{
+      /* border: 1px solid #FF4433; */
+      background: #FBFBFF;
+      color: #150F43;
+    }
+
+    .menu li{
+      padding: 1rem 0px;
+      margin: 0px 2rem;
+      border-bottom: 1px solid rgba(255,255,255,0.2);
+
+      a{
+        font-size: 2rem;
+      }
+    }
   }
 
   ul{
